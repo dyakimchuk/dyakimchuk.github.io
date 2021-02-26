f7561147c569aed66b0cbecdd86c137592bd2eb3
Denis Yakimchuk <denis.yakimchuk@gmail.com>
2021-02-26 18:20:24 +0200

    cv_js

---
+++ cv-data.js
@@ -0,0 +1,61 @@
+var data_technical = `
+<li>Knowledge of SDLC</li>
+<li>Good knowledge of bug life cycle</li>
+<li>Understanding of test design techniques</li>
+<li>Knowledge of Agile(Scrum&Kanban)</li>
+<li>Good practical experience BASH, SSH</li>
+<li>Practical experience with Apache, NGINX, MySQL.</li>
+<li>Understanding of programming languages (JS, TS, PHP)</li>
+<li>HTML/CSS</li>
+<li>Experience in working with Linux OS server (Ubuntu)</li>
+<li>Practical experience with instances at Digital Ocean, Google Cloud</li>
+<li>Experience of using version control system Git </li>
+<li>Some knowledge of AWS services</li>
+<li>Understanding of HTTP, TCP protocols</li>
+<li>Understanding of CI/CD </li>
+<li>Practical experience with REST APIs</li>
+<li>Understanding microservice based applications</li>
+<li>Hand-on experience with Docker</li>
+<li>Understanding basics of GDPR</li>
+`;
+
+var data_personal = `
+<li>Upper – intermediate English communication skills (B2)</li>
+<li>High attention to details and organisation</li>
+<li>Strong communication skills for work with different groups within the project</li>
+<li>Service and teamwork orientation</li>
+<li>Good analytical skills and strong attention to details</li>
+<li>Willingness to understand complex systems </li>
+<li>Self-motivated, self-disciplined and result-oriented</li>
+`;
+
+var data_pet = `
+<li>Automation with WDIO + mocha + chai + Allure</li>
+<li>API tests with Postman/newman</li>
+<li>On the server-side (Ubuntu on bare-metal): GGR, Selenoid, ReportPortal, Grafana, Prometheus, Alertmanager in Docker containers with Portainer</li>
+`;
+
+var data_exp = `
+<li>QA @ NDA, Part-time</li>
+<li>Oct 2018 – Aug 2020</li>
+<li></br></li>
+<li>Project:</li>
+<li>Complex infrastructure solution for project management, user permissions control and data synchronisation for DaVinci Resolve color grading software with web app and electron app.</li>
+<li></br></li>
+<li>Responsibilities:</li>
+<li>- Creating checklists and test cases.</li>
+<li>- Bug reporting and verification</li>
+<li>- Functional, regression, E2E, UI testing</li>
+<li>- Rest API testing</li>
+<li>- Executing test runs and analysing results</li>
+<li>- Manual FE performance auditing</li>
+`;
+
+var data_expect = `
+<li>Work on Web App</li>
+<li>Automation with WDIO</li>
+<li>Growth and education</li>
+<li>Ability to influence project technologies</li>
+<li>Continuous self-improvement</li>
+<li>Working using a Mac</li>
+`;
