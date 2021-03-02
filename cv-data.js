977155fb352e9b22c0f54ef1677bb078b20b4f6f
Denis Yakimchuk <denis.yakimchuk@gmail.com>
2021-03-02 18:09:38 +0200

    cv

--- cv-data.js
+++ cv-data.js
@@ -3,15 +3,17 @@ var data_technical = `
 <li>Good knowledge of bug life cycle</li>
 <li>Understanding of test design techniques</li>
 <li>Knowledge of Agile(Scrum&Kanban)</li>
+<li>Requirements testing/auditing/analys</li>
 <li>Good practical experience BASH, SSH</li>
-<li>Practical experience with Apache, NGINX, MySQL.</li>
+<li>Practical experience with Apache, NGINX, MySQL</li>
 <li>Understanding of programming languages (JS, TS, PHP)</li>
 <li>HTML/CSS</li>
-<li>Experience in working with Linux OS server (Ubuntu)</li>
+<li>Jira, Confluence</li>
+<li>Maintaining and administrating Linux OS servers</li>
 <li>Practical experience with instances at Digital Ocean, Google Cloud</li>
 <li>Experience of using version control system Git </li>
-<li>Some knowledge of AWS services</li>
-<li>Understanding of HTTP, TCP protocols</li>
+<li>Basic knowledge of AWS services</li>
+<li>Experience in HTTP, TCP protocols</li>
 <li>Understanding of CI/CD </li>
 <li>Practical experience with REST APIs</li>
 <li>Understanding microservice based applications</li>
@@ -30,14 +32,14 @@ var data_personal = `
 `;
 
 var data_pet = `
-<li>Automation with WDIO + mocha + chai + Allure</li>
+<li>E2E tests with WDIO + mocha + chai + Allure</li>
 <li>API tests with Postman/newman</li>
-<li>On the server-side (Ubuntu on bare-metal): GGR, Selenoid, ReportPortal, Grafana, Prometheus, Alertmanager in Docker containers with Portainer</li>
+<li>Infrastructure: (Ubuntu on bare-metal): GGR, Selenoid, ReportPortal, Grafana, Prometheus, Alertmanager in Docker.</li>
 `;
 
 var data_exp = `
 <li>QA @ NDA, Part-time</li>
-<li>Oct 2018 – Aug 2020</li>
+<li>Jan 2019 – Aug 2020</li>
 <li></br></li>
 <li>Project:</li>
 <li>Complex infrastructure solution for project management, user permissions control and data synchronisation for DaVinci Resolve color grading software with web app and electron app.</li>
@@ -53,9 +55,9 @@ var data_exp = `
 
 var data_expect = `
 <li>Work on Web App</li>
-<li>Automation with WDIO</li>
 <li>Growth and education</li>
+<li>Ability to write and maintain automation tests</li>
 <li>Ability to influence project technologies</li>
 <li>Continuous self-improvement</li>
-<li>Working using a Mac</li>
+<li>Improvement of automation skills</li>
 `;
