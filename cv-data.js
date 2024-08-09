86b42691e90f79321b3e41a1cc7f58d0986c18e4
lakymd01 <lakymd01@stepstone.com>
2024-08-09 13:46:14 +0300

    updates

--- cv-data.js
+++ cv-data.js
@@ -1,28 +1,38 @@
+const overview = `
+My excel in ensuring software products reach impeccable standards of quality and reliability.<br>
+My expertise lies in designing and executing cutting-edge automated test suites that ensure software products meet<br>
+optimal standards of quality and reliability, in case of issues or bugs collaborating closely with developers to swiftly resolve them.<br>
+I have keen attention to detail, analytical abilities, and critical thinking skills that drive me to enhance software quality continuously as well as refine and upgrade automated testing processes.<br>
+My clear communication and team player attitude empower me to work harmoniously with cross-functional teams helping them to deliver top-notch products that exceed stakeholders expectations.`
+
 const data_technical = `
-<li>Knowledge of SDLC</li>
-<li>Good knowledge of bug life cycle</li>
-<li>Understanding of test design techniques</li>
+<li>Strong knowledge of SDLC</li>
+<li>Strong knowledge of bug life cycle</li>
+<li>Requirements testing/auditing/analys</li>
+<li>Test design techniques</li>
+<li>Agile(Scrum&Kanban)</li>
 <li>Automation of functional, regression, E2E, UI testing of WEB app</li>
 <li>Rest API testing</li>
-<li>Reporting systems Allure, Report Portal</li>
-<li>Automation of FE performance auditing</li>
-<li>Knowledge of Agile(Scrum&Kanban)</li>
-<li>Requirements testing/auditing/analys</li>
-<li>Good practical experience BASH, SSH</li>
-<li>Practical experience with Apache, NGINX, MySQL</li>
-<li>Programming languages JS, TS</li>
-<li>HTML/CSS</li>
-<li>Bamboo, Jira, Confluence</li>
+<li>Performance testing</li>
+<li>Programming languages JS, TS, Python</li>
+<li>Knowledge of CI/CD pipelines and patterns</li>
+<li>Good practical experience Shell script, Apache, Nginx</li>
+<li>Git, Bitbucket version control systems and various CI/CD</li>
 <li>Maintaining and administrating Linux OS servers</li>
-<li>Practical experience with instances at Digital Ocean, Google Cloud</li>
-<li>Experience of using version control system Git, Bitbucket </li>
-<li>Basic knowledge of AWS services</li>
-<li>Experience in HTTP, TCP protocols</li>
-<li>Understanding of CI/CD </li>
-<li>Understanding microservice based applications</li>
-<li>Hand-on experience with Docker</li>
-<li>Understanding basics of GDPR</li>
+<li>Docker</li>
 `;
+
+const data_non_technical = `
+<li>Requirements testing/auditing/analys</li>
+<li>Agile(Scrum&Kanban)</li>
+<li>Jira, Confluence</li>
+<li>Test design techniques</li>
+<li>Concepts of GDPR</li>`
+
+const excited_about = `
+<li>Passionate about DevOps and building CI/CD and infrastructure</li>
+<li>Extreme sports and music</li>
+`
 
 const data_personal = `
 <li>Upper – intermediate English communication skills (B2)</li>
@@ -41,26 +51,14 @@ const data_position = `
 <li>Universum is the most recognised Employer Branding specialist in the world. Part of the StepStone Group and Axel Springer family, our diverse workforce is physically present in over 20 countries. We ensure employers around the world achieve their goals through data-driven Employer Branding. Consequently, this enables them to engage the talent they need to succeed.</li>
 <li></br></li>
 <li>Responsibilities:</li>
-<li>Creating test plan and defining criteria and definitions</li>
-<li>Creating infrastructure infrastructure for auto tests.</li>
-<li>Creating and supporting UI-E2E auto tests from a scratch.</li>
-<li>Creating service for data verification and validation.</li>
-<li>Creating and maintaining new API tests with Node.js.</li>
-<li>Maintaining legacy API tests with Python.</li>
+<li>Developing test plan and defining criteria and definitions</li>
+<li>Developing infrastructure for auto tests</li>
+<li>Developing and maintaining UI-E2E auto tests from a scratch</li>
+<li>Performance testing with K6</li>
+<li>Developing service for data verification and validation</li>
+<li>Developing API tests with Node.js</li>
 `;
 
 const data_stack = `
-<li>WDIO, Mocha, Chai, Allure, Got, Axios, Junit, Postman, Ubuntu, Docker, GGR, Selenoid, ReportPortal, Grafana, Prometheus, Alertmanager … and more.</li>
-`;
-
-const data_additional = `
-<li>Requirements testing/auditing/analys</li>
-<li>Agile(Scrum&Kanban)</li>
-<li>Jira, Confluence</li>
-<li>Knowledge of SDLC</li>
-<li>Good knowledge of bug life cycle</li>
-<li>Test design techniques</li>
-<li>Understanding micro-service based applications</li>
-<li>Basics of GDPR</li>
-<li>Passionate on DevOps learning to build CI/CD and infrastructure with AWS.</li>
+<li>WDIO, Mocha, K6, Chai, Allure, Got, Axios, Junit, Postman, AWS, Ubuntu, Docker, GGR, Selenoid, ReportPortal, Grafana, Prometheus, Alertmanager … and more.</li>
 `;
