b0f17dd5afe92bc23d9f45294d93157cce3b3fe1
lakymd01 <lakymd01@stepstone.com>
2025-03-16 14:28:08 +0200

    cv_update_2024

--- cv-data.js
+++ cv-data.js
@@ -1,33 +1,31 @@
-const overview = `
-My excel in ensuring software products reach impeccable standards of quality and reliability.<br>
-My expertise lies in designing and executing cutting-edge automated test suites that ensure software products meet<br>
-optimal standards of quality and reliability, in case of issues or bugs collaborating closely with developers to swiftly resolve them.<br>
-I have keen attention to detail, analytical abilities, and critical thinking skills that drive me to enhance software quality continuously as well as refine and upgrade automated testing processes.<br>
-My clear communication and team player attitude empower me to work harmoniously with cross-functional teams helping them to deliver top-notch products that exceed stakeholders expectations.`
+const overview = '\tMy expertise lies in designing, developing and executing automated test suites that ensure software products meet optimal standards of quality and reliability, in case of issues or bugs, collaborating closely with developers to swiftly resolve them.\n' +
+    '\tI have keen attention to detail, analytical abilities, and critical thinking skills that drive me to enhance software quality continuously as well as refine and upgrade automated testing processes.\n' +
+    '\tMy collaboration and team player attitude empower me to work harmoniously with cross-functional teams helping them to deliver top-notch products that meet stakeholders expectations.'
 
 const data_technical = `
-<li>Strong knowledge of SDLC</li>
-<li>Strong knowledge of bug life cycle</li>
-<li>Requirements testing/auditing/analys</li>
-<li>Test design techniques</li>
-<li>Agile(Scrum&Kanban)</li>
-<li>Automation of functional, regression, E2E, UI testing of WEB app</li>
-<li>Rest API testing</li>
-<li>Performance testing</li>
-<li>Programming languages JS, TS, Python</li>
-<li>Knowledge of CI/CD pipelines and patterns</li>
-<li>Good practical experience Shell script, Apache, Nginx</li>
-<li>Git, Bitbucket version control systems and various CI/CD</li>
-<li>Maintaining and administrating Linux OS servers</li>
-<li>Docker</li>
+<li>Automation of functional, regression, E2E, UI testing of WEB app;</li>
+<li>REST API testing;</li>
+<li>Performance testing with K6;</li>
+<li>Programming languages JavaScript, TypeScript;</li>
+<li>Strong knowledge of SDLC;</li>
+<li>Knowledge of CI/CD pipelines and patterns;</li>
+<li>Strong knowledge of bug life cycle;</li>
+<li>Docker;</li>
+<li>Good practical experience of Shell script, Apache, Nginx;</li>
+<li>Git, Bitbucket version control systems, and various CI/CD;</li>
+<li>Maintaining and administrating Unix OS servers;</li>
+<li>Building UI Browser tests from the zero.</li>
+<li>Building API Tests from scratch.</li>
+<li>Developing k6 Performance tests from the zero.</li>
 `;
 
 const data_non_technical = `
-<li>Requirements testing/auditing/analys</li>
-<li>Agile(Scrum&Kanban)</li>
-<li>Jira, Confluence</li>
-<li>Test design techniques</li>
-<li>Concepts of GDPR</li>`
+<li>Requirements testing/auditing/analysis;</li>
+<li>Agile(Scrum&Kanban);</li>
+<li>Jira, Confluence;</li>
+<li>Test design techniques;</li>
+<li>Concepts of GDPR;</li>
+`
 
 const excited_about = `
 <li>Passionate about DevOps and building CI/CD and infrastructure</li>
@@ -37,28 +35,31 @@ const excited_about = `
 const data_personal = `
 <li>Upper – intermediate English communication skills (B2)</li>
 <li>High attention to details and organisation</li>
-<li>Strong communication skills for work with different groups within the project </li>
-<li>Service and teamwork orientation
+<li>Strong communication skills for work with different groups within the project</li>
+<li>Service and teamwork orientation</li>
 <li>Good analytical skills</li>
 <li>Willingness to understand complex systems</li>
 <li>Self-motivated, self-disciplined and result-oriented</li>
 `;
 
 const data_position = `
-<li>QA Automation Engineer @ <a href="https://universumglobal.com/" target="_blank">Universum Global</a></li>
+<li>Senior QA Automation Engineer @ <a href="https://universumglobal.com/" target="_blank">Universum Global</a></li>
 <li>Since Apr 2021</li>
 <li></br></li>
 <li>Universum is the most recognised Employer Branding specialist in the world. Part of the StepStone Group and Axel Springer family, our diverse workforce is physically present in over 20 countries. We ensure employers around the world achieve their goals through data-driven Employer Branding. Consequently, this enables them to engage the talent they need to succeed.</li>
 <li></br></li>
 <li>Responsibilities:</li>
-<li>Developing test plan and defining criteria and definitions</li>
-<li>Developing infrastructure for auto tests</li>
-<li>Developing and maintaining UI-E2E auto tests from a scratch</li>
+<li>Developing test strategy, test plan and defining criteria and definitions.</li>
+<li>Developing and maintaining infrastructure for auto tests - Bamboo, AWS.</li>
+<li>Building framework and maintaining UI-E2E auto tests from scratch (Webdriver.io).</li>
+<li>Developing API tests from scratch.</li>
 <li>Performance testing with K6</li>
-<li>Developing service for data verification and validation</li>
-<li>Developing API tests with Node.js</li>
+<li>Developing a service for data verification and validation.</li>
+<li>Daily reporting of current project status.</li>
+<li>Execution and verification of delivery process.</li>
+<li>Mentoring of Junior QA Automation specialist.</li>
 `;
 
 const data_stack = `
-<li>WDIO, Mocha, K6, Chai, Allure, Got, Axios, Junit, Postman, AWS, Ubuntu, Docker, GGR, Selenoid, ReportPortal, Grafana, Prometheus, Alertmanager … and more.</li>
+<li>Playwright, WDIO, Mocha, K6, Chai, Allure, Got, Axios, Grafana, Postman, AWS, Ubuntu, Docker, ReportPortal, Prometheus, Alertmanager, SQL, Tableau … and more.</li>
 `;
